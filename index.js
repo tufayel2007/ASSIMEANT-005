7e8c5cac7e8d153b6fffca5784dc72cdbde1fe09
tufayel2007 <ta07092007@gmail.com>
2025-08-28 16:45:12 +0600

    functions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function copyNumber(number) {
-  navigator.clipboard.writeText(number);
-  alert(number + " কপি করা হয়েছে ✅");
-}
+let loveCounter = 0;
+let copyCounter = 0;
+let starCount = 100;
+
+// ====== Call Function  *======
 function makeCall(service, number) {
   const historyList = document.getElementById("historyList");
 
@@ -10,30 +11,49 @@ function makeCall(service, number) {
     "border:1px solid gray;padding:6px;margin-bottom:4px;border-radius:6px";
 
   li.innerHTML = `
-    <div style="display:flex;justify-content:space-between;font-weight:bold">
-      <span>${service}</span>
-      <span style="color:gray">${new Date().toLocaleTimeString()}</span>
-    </div>
-    <div>${number}</div>
-  `;
+      <div style="display:flex;justify-content:space-between;font-weight:bold">
+        <span>${service}</span>
+        <span style="color:gray">${new Date().toLocaleTimeString()}</span>
+      </div>
+      <div>${number}</div>
+    `;
 
   historyList.prepend(li);
+
+  // ⭐  add
+  addStars(20);
+
+  alert(service - " এ কল করা হয়েছে ✅");
 }
 
+// ====== Call History Clear ======
 function clearHistory() {
   document.getElementById("historyList").innerHTML = "";
 }
-// loveCounter
-let loveCounter = 0;
 
+// ====== Love Counter ======
 function increaseLove() {
   loveCounter++;
   document.getElementById("loveCount").textContent = loveCounter;
 }
-// cpppy counter
-let copyCounter = 0;
-function copyNumber(number) {
-  navigator.clipboard.writeText(number);
-  copyCounter++;
-  document.getElementById("copyCounter").textContent = copyCounter;
+
+// ====== Copy Counter ======
+function copyNumber(num) {
+  navigator.clipboard
+    .writeText(num)
+    .then(() => {
+      copyCounter++;
+      document.getElementById("copyCounter").textContent = copyCounter;
+      alert(num + " কপি করা হয়েছে ✅");
+    })
+    .catch((err) => {
+      console.error("কপি করতে সমস্যা হয়েছে:", err);
+    });
+}
+
+// ====== ⭐ Star Function ======
+function addStars(amount) {
+  starCount -= amount;
+  if (starCount < 0) starCount = 0;
+  document.getElementById("starCount").innerText = starCount;
 }
